52cd2132da5af0e41fc739513a54f429c3917cdc
Daniel Malone <danieljmalone@gmail.com>
2017-07-29 16:30:46 -0500

    make fragment replacement work

--- app/src/main/java/com/finepointmobile/androidfragments/MainActivity.java
+++ app/src/main/java/com/finepointmobile/androidfragments/MainActivity.java
@@ -2,8 +2,10 @@ package com.finepointmobile.androidfragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by danielmalone on 7/29/17.
@@ -11,13 +13,30 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton mFab;
+    FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_main, new MainFragment());
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_main, new MainFragment(), "original_fragment");
         fragmentTransaction.commit();
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                FragmentTransaction newFragment = getSupportFragmentManager().beginTransaction();
+                newFragment.replace(R.id.fragment_main, new FragmentContact(), "contact_fragment");
+                newFragment.commit();
+            }
+        });
     }
 }

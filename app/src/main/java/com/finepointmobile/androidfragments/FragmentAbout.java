cdc2170a0f97e7ed9adc599cc289880931a76aaa
Daniel Malone <danieljmalone@gmail.com>
2017-07-29 17:06:15 -0500

    make fragment do something

--- app/src/main/java/com/finepointmobile/androidfragments/FragmentAbout.java
+++ app/src/main/java/com/finepointmobile/androidfragments/FragmentAbout.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by danielmalone on 7/29/17.
@@ -13,9 +14,24 @@ import android.view.ViewGroup;
 
 public class FragmentAbout extends Fragment {
 
+    Button mButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mButton = (Button) view.findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mButton.setText("new mButton!");
+            }
+        });
     }
 }
--- app/src/main/java/com/finepointmobile/androidfragments/MainActivity.java
+++ app/src/main/java/com/finepointmobile/androidfragments/MainActivity.java
@@ -3,7 +3,6 @@ package com.finepointmobile.androidfragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -29,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: here");
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.fragment_main, new FragmentAbout(), "fragment_about");
-                ft.commit();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_main, new FragmentAbout(), "fragment_about")
+                        .commit();
 
                 FragmentAbout fragmentAbout = (FragmentAbout) getSupportFragmentManager().findFragmentByTag("fragment_about");
                 Log.d(TAG, "onClick: fragment: " + fragmentAbout);
